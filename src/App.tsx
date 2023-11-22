b89737fbab386e6c52e02fc6b56c6fc839ab887e
arifmia1129 <arifmia1129@gmail.com>
2023-11-22 10:55:24 +0600

    conditional render login and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Toaster } from "react-hot-toast";
 import Login from "./Pages/Authentication/Login";
 import LoginNoticeModal from "./components/modal/LoginNoticeModal";
+import { useAppSelector } from "./redux/hooks";
+import Home from "./Pages/Home/Home";
+import Loading from "./components/shared/Loading";
 
 function App() {
+  const { userName, isLoading } = useAppSelector((state) => state.user);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <>
-      <Login />
+      {userName ? <Home /> : <Login />}
       <LoginNoticeModal />
       {/* react hot toast */}
       <Toaster />
